c10ce38665edf3b5303c38e534ba43b4460e6c16
harikowshik.molugu <harikowshik.molugu@gmail.com>
2023-09-02 14:01:57 +0530

    updated

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -15,15 +15,7 @@ function Navbar() {
     navRef.current.classList.toggle("responsive_nav");
   };
 
-  const header = document.getElementById('header');
-
-window.addEventListener('scroll', () => {
-  if (window.scrollY > 0) {
-    header.classList.add('header-shadow');
-  } else {
-    header.classList.remove('header-shadow');
-  }
-});
+  
 
 
   return (
